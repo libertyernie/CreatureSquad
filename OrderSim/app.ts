48ae333852682aa1b5ab2f67efd6c292135b828d
libertyernie <maybeway36@gmail.com>
2017-04-29 08:14:09 -0500

    First commit

---
+++ OrderSim/app.ts
@@ -0,0 +1,154 @@
+﻿const Lineup = [new Batter("Dexter Fowler", {
+    at_bats: 456,
+    walks: 79,
+    hit_by_pitch: 11,
+    sacrifice_hits: 1,
+    sacrifice_flies: 4,
+    total_hits: 126,
+    doubles: 25,
+    triples: 7,
+    home_runs: 13,
+    intentional_walks: 0
+}), new Batter("Kris Bryant", {
+    at_bats: 603,
+    walks: 75,
+    hit_by_pitch: 18,
+    sacrifice_hits: 0,
+    sacrifice_flies: 3,
+    total_hits: 176,
+    doubles: 35,
+    triples: 3,
+    home_runs: 39,
+    intentional_walks: 5 
+}), new Batter("Anthony Rizzo", {
+    at_bats: 583,
+    walks: 74,
+    hit_by_pitch: 16,
+    sacrifice_hits: 0,
+    sacrifice_flies: 3,
+    total_hits: 170,
+    doubles: 43,
+    triples: 4,
+    home_runs: 32,
+    intentional_walks: 8
+}), new Batter("Ben Zobrist", {
+    at_bats: 523,
+    walks: 96,
+    hit_by_pitch: 4,
+    sacrifice_hits: 4,
+    sacrifice_flies: 4,
+    total_hits: 142,
+    doubles: 31,
+    triples: 3,
+    home_runs: 18,
+    intentional_walks: 6
+}), new Batter("Addison Russell", {
+    at_bats: 525,
+    walks: 55,
+    hit_by_pitch: 12,
+    sacrifice_hits: 0,
+    sacrifice_flies: 6,
+    total_hits: 125,
+    doubles: 25,
+    triples: 3,
+    home_runs: 21,
+    intentional_walks: 6
+}), new Batter("Jorge Soler", {
+    at_bats: 227,
+    walks: 31,
+    hit_by_pitch: 3,
+    sacrifice_hits: 0,
+    sacrifice_flies: 3,
+    total_hits: 54,
+    doubles: 9,
+    triples: 0,
+    home_runs: 12,
+    intentional_walks: 0
+}), new Batter("Jason Heyward", {
+    at_bats: 530,
+    walks: 54,
+    hit_by_pitch: 5,
+    sacrifice_hits: 1,
+    sacrifice_flies: 2,
+    total_hits: 122,
+    doubles: 27,
+    triples: 1,
+    home_runs: 7,
+    intentional_walks: 0
+}), new Batter("Miguel Montero", {
+    at_bats: 241,
+    walks: 38,
+    hit_by_pitch: 3,
+    sacrifice_hits: 0,
+    sacrifice_flies: 2,
+    total_hits: 52,
+    doubles: 8,
+    triples: 1,
+    home_runs: 8,
+    intentional_walks: 5
+}), new Batter("Jon Lester", {
+    at_bats: 59,
+    walks: 6,
+    hit_by_pitch: 0,
+    sacrifice_hits: 10,
+    sacrifice_flies: 0,
+    total_hits: 6,
+    doubles: 3,
+    triples: 0,
+    home_runs: 0,
+    intentional_walks: 0
+})];
+
+class ViewModel {
+    readonly bases: KnockoutObservable<Bases>;
+    readonly runs: KnockoutObservable<number>;
+    readonly outs: KnockoutObservable<number>;
+    readonly inning: KnockoutObservable<number>;
+
+    readonly lineupIndex: KnockoutObservable<number>;
+    readonly batter: KnockoutComputed<Batter>;
+    readonly lastResult: KnockoutObservable<string>;
+
+    constructor() {
+        this.bases = ko.observable(new Bases());
+        this.runs = ko.observable(0);
+        this.outs = ko.observable(0);
+        this.inning = ko.observable(1);
+
+        this.lineupIndex = ko.observable(0);
+        this.batter = ko.pureComputed(() => Lineup[this.lineupIndex() % Lineup.length]);
+        this.lastResult = ko.observable("-");
+    }
+
+    bat() {
+        let total_batters_expected = this.bases().getCount() + 1;
+        let result1 = this.batter().bat();
+        this.lastResult(`${this.batter().name} - ${BatterResult[result1]}`);
+        let result = new PlateAppearanceResult(this.batter(), this.bases(), result1);
+        let total_batters_actual = result.basesResult.bases.getCount() + result.basesResult.runs_scored + (result.out ? 1 : 0);
+        if (total_batters_expected != total_batters_actual) {
+            console.log(result);
+            throw new Error(`Expected ${total_batters_expected}, got ${total_batters_actual}`);
+        }
+        this.bases(result.basesResult.bases);
+        if (result.out) {
+            this.outs(this.outs() + 1);
+        }
+        if (this.outs() >= 3) {
+            this.bases(new Bases());
+            this.outs(0);
+            this.inning(this.inning() + 1);
+        } else {
+            if (result.basesResult.runs_scored > 0) {
+                this.runs(this.runs() + result.basesResult.runs_scored);
+            }
+        }
+        this.lineupIndex(this.lineupIndex() + 1);
+    }
+}
+
+window.onload = () => {
+    var el = document.getElementById('content');
+    var viewModel = new ViewModel();
+    ko.applyBindings(viewModel, el);
+};
---
+++ OrderSim/party-member.ts
@@ -0,0 +1,255 @@
+﻿interface OffensiveAverages {
+    at_bats: number;
+    total_hits: number;
+    doubles: number;
+    triples: number;
+    home_runs: number;
+    walks: number;
+    //double_plays: number;
+    hit_by_pitch: number;
+    sacrifice_hits: number;
+    sacrifice_flies: number;
+    intentional_walks: number;
+}
+
+interface PlateAppearanceAverages {
+    plate_appearances: number;
+    singles: number;
+    doubles: number;
+    triples: number;
+    home_runs: number;
+    intentional_walks: number;
+    unintentional_walks: number;
+    hit_by_pitch: number;
+    //double_plays: number;
+    sacrifice_hits: number;
+    sacrifice_flies: number;
+    other_outs: number;
+}
+
+function calculateAverages(a: OffensiveAverages): PlateAppearanceAverages {
+    return {
+        plate_appearances: a.at_bats + a.walks + a.hit_by_pitch + a.sacrifice_hits + a.sacrifice_flies,
+        singles: a.total_hits - a.doubles - a.triples - a.home_runs,
+        unintentional_walks: a.walks - a.intentional_walks,
+        other_outs: a.at_bats - a.total_hits,
+        ...a
+    };
+}
+
+class Bases {
+    constructor(
+        readonly first?: Batter | null,
+        readonly second?: Batter | null,
+        readonly third?: Batter | null
+    ) { }
+
+    walk(batter: Batter) {
+        if (this.first == null) {
+            return {
+                bases: new Bases(batter, this.second, this.third),
+                runs_scored: 0
+            };
+        } else if (this.second == null) {
+            return {
+                bases: new Bases(batter, this.first, this.third),
+                runs_scored: 0
+            };
+        } else if (this.third == null) {
+            return {
+                bases: new Bases(batter, this.second, this.third),
+                runs_scored: 0
+            };
+        } else {
+            return {
+                bases: new Bases(batter, this.second, this.third),
+                runs_scored: 1
+            };
+        }
+    }
+
+    advanceOne(first?: Batter): BasesResult {
+        console.log("1");
+        return {
+            bases: new Bases(first, this.first, this.second),
+            runs_scored: this.third ? 1 : 0
+        };
+    }
+
+    advanceTwo(first?: Batter, second?: Batter) {
+        console.log("2");
+        return {
+            bases: new Bases(first, second, this.first),
+            runs_scored: (this.second ? 1 : 0) + (this.third ? 1 : 0)
+        };
+    }
+
+    advanceThree(first?: Batter, second?: Batter, third?: Batter) {
+        console.log("3");
+        return {
+            bases: new Bases(first, second, third),
+            runs_scored: (this.first ? 1 : 0) + (this.second ? 1 : 0) + (this.third ? 1 : 0)
+        };
+    }
+
+    advanceAll() {
+        console.log("4");
+        return {
+            bases: new Bases(),
+            runs_scored: (this.first ? 1 : 0) + (this.second ? 1 : 0) + (this.third ? 1 : 0) + 1
+        };
+    }
+
+    getCount() {
+        return (this.first ? 1 : 0) + (this.second ? 1 : 0) + (this.third ? 1 : 0);
+    }
+}
+
+interface BasesResult {
+    bases: Bases;
+    runs_scored: number;
+}
+
+class PlateAppearanceResult {
+    readonly basesResult: BasesResult;
+    readonly out: boolean;
+
+    constructor(batter: Batter, bases: Bases, br: BatterResult) {
+        switch (br) {
+            case BatterResult.Single:
+                this.basesResult = bases.advanceOne(batter);
+                break;
+            case BatterResult.Single_ExtraBase:
+                this.basesResult = bases.advanceTwo(batter);
+                break;
+            case BatterResult.Double:
+                this.basesResult = bases.advanceTwo(null, batter);
+                break;
+            case BatterResult.Double_ExtraBase:
+                this.basesResult = bases.advanceThree(null, batter);
+                break;
+            case BatterResult.Triple:
+                this.basesResult = bases.advanceThree(null, null, batter);
+                break;
+            case BatterResult.HomeRun:
+                this.basesResult = bases.advanceAll();
+                break;
+            case BatterResult.Walk:
+            case BatterResult.HitByPitch:
+                this.basesResult = bases.walk(batter);
+                break;
+            case BatterResult.SacrificeFly:
+            case BatterResult.Out_ExtraBase:
+                this.basesResult = bases.advanceOne(null);
+                this.out = true;
+                break;
+            case BatterResult.Out:
+                this.basesResult = {
+                    bases: bases,
+                    runs_scored: 0
+                };
+                this.out = true;
+                break;
+        }
+    }
+}
+
+enum BatterResult {
+    Single,
+    Single_ExtraBase,
+    Double,
+    Double_ExtraBase,
+    Triple,
+    HomeRun,
+    Walk,
+    HitByPitch,
+    SacrificeFly,
+    Out,
+    Out_ExtraBase
+}
+
+class Batter {
+    private averages: PlateAppearanceAverages;
+
+    constructor(
+        readonly name: string,
+        averages: OffensiveAverages
+    ) {
+        this.averages = calculateAverages(averages);
+        console.log(this.averages);
+    }
+
+    bat(): BatterResult {
+        let total = this.averages.plate_appearances
+            - this.averages.intentional_walks
+            - this.averages.sacrifice_hits;
+
+        let random = Math.random() * total;
+        let walk = 0;
+
+        walk += this.averages.singles;
+        if (walk >= random) {
+            if (Math.random() < 0.2) {
+                console.log("Single (1st to 3rd)");
+                return BatterResult.Single_ExtraBase;
+            } else {
+                console.log("Single");
+                return BatterResult.Single;
+            }
+        }
+
+        walk += this.averages.doubles;
+        if (walk >= random) {
+            if (Math.random() < 0.2) {
+                console.log("Double (score from 1st)");
+                return BatterResult.Double_ExtraBase;
+            } else {
+                console.log("Double");
+                return BatterResult.Double;
+            }
+        }
+
+        walk += this.averages.triples;
+        if (walk >= random) {
+            console.log("Triple");
+            return BatterResult.Triple;
+        }
+
+        walk += this.averages.home_runs;
+        if (walk >= random) {
+            console.log("Home run");
+            return BatterResult.HomeRun;
+        }
+
+        walk += this.averages.unintentional_walks;
+        if (walk >= random) {
+            console.log("Walk");
+            return BatterResult.Walk;
+        }
+
+        walk += this.averages.hit_by_pitch;
+        if (walk >= random) {
+            console.log("HBP");
+            return BatterResult.HitByPitch;
+        }
+
+        walk += this.averages.sacrifice_flies;
+        if (walk >= random) {
+            console.log("Sac fly");
+            return BatterResult.SacrificeFly;
+        }
+
+        walk += this.averages.other_outs;
+        if (walk >= random) {
+            if (Math.random() < 0.2) {
+                console.log("Out (baserunners advance)");
+                return BatterResult.Out_ExtraBase;
+            } else {
+                console.log("Out");
+                return BatterResult.Out;
+            }
+        }
+
+        throw new Error("Invalid probability");
+    }
+}
