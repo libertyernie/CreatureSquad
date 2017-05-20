be1599e668543e02befe3580b8568b4bd26d5d67
libertyernie <maybeway36@gmail.com>
2017-05-20 12:12:02 -0500

    Update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ This file in the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+const wbBuild = require('workbox-build');
 
-gulp.task("default", function () {
+gulp.task("ts", function () {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("."));
 });
+
+gulp.task("default", gulp.series("ts"));
