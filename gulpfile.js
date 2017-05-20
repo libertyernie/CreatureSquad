17ce4b15873e354131d0bcd6e65128f9db061190
libertyernie <maybeway36@gmail.com>
2017-05-20 12:01:16 -0500

    Add package.json and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("default", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("."));
+});
